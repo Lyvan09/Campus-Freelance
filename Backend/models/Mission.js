360e1a9bd579b00bdb21c0575a3a2b99b20319f5
Lyvan <nyalyvan2@gmail.com>
2026-06-16 17:03:59 +0100

    feat: Ajout du système d'authentification, rôles et historique des missions

---
+++ Backend/models/Mission.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const missionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  status: {
+    type: String,
+    enum: ['Ouverte', 'Pourvue'],
+    default: 'Ouverte',
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Mission', missionSchema);
